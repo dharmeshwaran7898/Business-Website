7d243a5bf960fc473f1b7cb648815beb776ac23f
dharmeshwaran7898 <dharmeshwaran7898@gmail.com>
2023-01-08 22:54:05 +0530

    comited_on_08_01_23

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="Home">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro atque
+        delectus voluptate? Incidunt, at nam? Voluptatem, illo asperiores magnam
+        ipsam sit dicta, laboriosam hic labore sunt, voluptatum fuga nisi
+        molestiae. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Quos, repellat! Animi, atque nobis inventore ipsam saepe vero
+        exercitationem recusandae ipsa illo eum aliquid fugit molestias amet
+        corrupti laudantium natus quisquam.
+      </div>
+    </>
+  );
+};
+
+export default Home;
